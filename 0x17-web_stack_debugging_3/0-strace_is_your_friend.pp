c79602c029c61afc099e09fc1d0cf65b96b88982
integralxdx <starbruce1@gmail.com>
2022-12-04 07:54:11 +0100

    dd

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-exec { '/var/www/html/wp-setting.php':
-  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
